86d50b272c126dfa26121f06d98d1c5ffd8e6344
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-05-09 00:50:43 +0530

    hashed user password and stored in db

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
 
 
 const userSchema = new mongoose.Schema({
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.models.users || mongoose.model('users',userSchema);
 
-export default User ;
+module.exports = User ;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,7 @@
 const { UserRepository } = require('../repositories');
 const { StatusCodes } = require('http-status-codes');
 const AppError = require('../utils/errors/app-error');
-
+const bcryptjs = require('bcryptjs');
 
 const userRepository = new UserRepository();
 
@@ -9,6 +9,9 @@ const userRepository = new UserRepository();
 async function signup(data)
 {
     try {
+        const {username, email, password} = data;
+        console.log('request data : ',data);
+
         // checking if user is exist or not
         const isUser = await userRepository.getUserByEmail(data.email);
 
@@ -18,9 +21,17 @@ async function signup(data)
             console.log('User is already exist ');
             return { exist:true,message : `User with ${data.email} is already exist `}
         }
-           
+        
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        const newUser_Payload = {
+            username,
+            email,
+            password:hashedPassword,
+        };
+    
         // creating new user
-        const user = await userRepository.create(data);
+        const user = await userRepository.create(newUser_Payload);
         console.log('user created :',user);
 
         return user;
