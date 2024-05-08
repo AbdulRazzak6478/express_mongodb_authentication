3637f22ad1c2374956c504461a5a7c462737b85c
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-05-08 23:50:55 +0530

    server is started

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const { ServerConfig} = require('./config/')
+
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(ServerConfig.PORT,async()=>{
+    console.log(`Successfully started the server on PORT:${ServerConfig.PORT}`);
+    console.log('Server is running on http://localhost:'+ServerConfig.PORT);
+});
